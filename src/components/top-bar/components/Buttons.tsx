22d2575c4a7b860d48274463dff124ce625d490e
Florian Laptop <floriandollani15@gmail.com>
2025-01-16 22:43:21 +0100

    feat: :sparkles: badge icon at shopping cart

--- src/components/top-bar/components/Buttons.tsx
+++ src/components/top-bar/components/Buttons.tsx
@@ -1,7 +1,8 @@
-import { IconButton, Stack } from '@mui/joy';
+import { Badge, IconButton, Stack } from '@mui/joy';
 import { lazy, Suspense } from 'react';
 import { MdOutlineShoppingCart as CartIcon } from 'react-icons/md';
 import { useLocation } from 'react-router-dom';
+import useCartStore from 'src/features/shopping-cart/stores/shoppingCart';
 import useWindowDimensions from 'src/hooks/useWindowsDimesions';
 
 const CategoryButton = lazy(
@@ -12,6 +13,8 @@ export default function Buttons() {
     const { pathname } = useLocation();
     const { width } = useWindowDimensions();
 
+    const { cartItems } = useCartStore();
+
     const handleToggleCart = async () => {
         const useRenderStore = await import('src/stores/render');
         useRenderStore.default.getState().toggleIsShoppingCartVisible();
@@ -19,18 +22,23 @@ export default function Buttons() {
 
     return (
         <Stack direction={{ md: 'row' }} gap={{ md: '0.938rem' }}>
-            <IconButton
-                disabled={pathname === '/cart'}
-                variant='solid'
-                size={width < 900 ? 'sm' : 'xs'}
-                color='primary'
-                sx={(theme) => ({
-                    bgcolor: theme.palette.primary[900],
-                })}
-                onClick={handleToggleCart}
+            <Badge
+                badgeContent={cartItems && cartItems.length > 0 ? '' : 0}
+                size={width < 900 ? 'md' : 'sm'}
             >
-                <CartIcon />
-            </IconButton>
+                <IconButton
+                    disabled={pathname === '/cart'}
+                    variant='solid'
+                    size={width < 900 ? 'sm' : 'xs'}
+                    color='primary'
+                    sx={(theme) => ({
+                        bgcolor: theme.palette.primary[900],
+                    })}
+                    onClick={handleToggleCart}
+                >
+                    <CartIcon />
+                </IconButton>
+            </Badge>
 
             {width < 899 ? (
                 ''
